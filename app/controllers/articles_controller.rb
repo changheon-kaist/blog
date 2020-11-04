20a86b30de356e3676c4a5cf841af7390426fd3b
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-11-04 21:29:47 +0900

    rails generate model Article !

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def create
+    @article = Article.new(params[:article])
+
+    @article.save
+    # returns boolean true: saved, false: not saved
+    redirect_to @article
+  end
 end
