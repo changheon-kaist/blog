f263caa405d386c1319705afc70f8d068444f423
changheon.kim <changheon.kim@purplelabs.co.kr>
2020-11-04 22:35:44 +0900

    Article의 기본 액션과 view bootstrapping
    - index
      - 모든 article들을 테이블 형태로 보여줌.
      - 각 article show 페이지 이동 링크 포함
      - new 액션 링크 포함
    - show
      - article의 내용을 보여줌
      - 뒤로가기 (index로)
    - create
      - Strong parameter 적용
      - 뒤로가기 (index로)
    
    ++ Welcome(root) 에서 articles(index)로 링크 추가
    
    필수적인 동작과 뷰를 bootstrapping 하였음.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,26 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @article = Article.new(params[:article])
-
+    @article = Article.new(article_params)
     @article.save
     # returns boolean true: saved, false: not saved
+
     redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
